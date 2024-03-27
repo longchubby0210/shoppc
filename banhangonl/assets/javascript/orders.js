7a09d0637466e15eb657b969ff150b48f309916e
NGUYEN DUC LONG <138957715+longchubby0210@users.noreply.github.com>
2024-03-27 20:06:02 +0700

    update

---
+++ banhangonl/assets/javascript/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Laptop gaming Acer Nitro 5 AN515 58 52SP',
+        productNumber: '85',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Laptop gaming MSI Sword 16 HX B14VFKG 045VN',
+        productNumber: '97',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Laptop Lenovo V14 G4 IAH 83FR000UVN',
+        productNumber: '36',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
